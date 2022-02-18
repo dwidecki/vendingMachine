6b33d1b3277ff344ab38012cb604ac3892115fae
dwidecki <dylan.widecki@quinnipiac.edu>
2022-02-18 11:34:38 -0500

    The user is welcomed and given instructions at the beginning of the transaction

--- src/vendingMachine/vendingMachine.java
+++ src/vendingMachine/vendingMachine.java
@@ -1,7 +1,29 @@
 package vendingMachine;
 
 public class vendingMachine {
+	
 	public static void main(String[] args) {
+		
+		//Welcome message and instructions
+		System.out.println("Welcome to our vending machine");
+		System.out.println("Please enter the number associated with your snack choice");
+		
+		//Add snack choices
+		
+		//Add price for each snack
+		
+		//Make it so user can choose a snack option
+		
+		//print total price for snack selected and ask user to insert $1 bills
+		
+		//confirmation message that asks user to confirm or cancel the transaction, if cancelled full refund
+		
+		//logic for calculating exact change given back to user if too much is given
+		
+		//departure message
+
+		
+		
 		
 	}
 
